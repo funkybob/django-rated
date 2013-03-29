cb86a53e0784a37e651fc3a977dce1edfd03c1d6
Curtis <curtis@tinbrain.net>
2013-03-29 19:36:06 +1100

    Administrativa

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-rated',
+    version='0.0.1',
+    description='A rate limiting middleware for Django',
+    author='Curtis Maloney',
+    author_email='curtis@tinbrain.net',
+    url='http://github.com/funkybob/django-rated',
+    keywords=['django', 'api',],
+    packages = find_packages(exclude=['test.*']),
+    zip_safe=False,
+    classifiers = [
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    requires = [
+        'Django (>=1.5)',
+    ],
+)
