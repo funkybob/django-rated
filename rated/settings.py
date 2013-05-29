4aecaea25de1aeb7b2fe2a718ab3701887a877cf
Curtis Maloney <curtis@tinbrain.net>
2013-05-29 11:33:32 +1000

    Fixed typo

--- rated/settings.py
+++ rated/settings.py
@@ -2,7 +2,7 @@
 from django.conf import settings
 
 DEFAULT_REALM = getattr(settings, 'RATED_DEFAULT_REALM', 'default')
-DEFAILT_LIMIT = getattr(settings, 'RATED_DEFAULT_LIMIT', 100)
+DEFAULT_LIMIT = getattr(settings, 'RATED_DEFAULT_LIMIT', 100)
 DEFAULT_TIMEOUT = getattr(settings, 'RATED_DEFAULT_TIMEOUT', 60 * 60)
 
 REALMS = getattr(settings, 'RATED_REALMS', {})
