31e81eab151ee68a41d678a25365ee3fde1a9bb7
Curtis <curtis@tinbrain.net>
2013-08-28 10:12:26 +1000

    Bumped version to 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-rated',
-    version='0.0.3',
+    version='1.0.0',
     description='A rate limiting middleware for Django',
     author='Curtis Maloney',
     author_email='curtis@tinbrain.net',
