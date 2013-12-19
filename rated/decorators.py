ec613fb853113ff1b266aad19ea1e2a5a95aae87
funkybob <curtis@tinbrain.net>
2013-12-19 13:47:48 +1100

    Added rate_limit decorator

--- rated/decorators.py
+++ rated/decorators.py
@@ -1,6 +1,7 @@
 
-from functools import partial
+from functools import partial, wraps
 
+from .middleware import RatedMiddleware
 from .settings import DEFAULT_REALM
 
 def rated_realm(func=None, realm=None):
@@ -9,4 +10,18 @@ def rated_realm(func=None, realm=None):
         return partial(rated_realm, realm=realm)
     func._rated_realm = realm or DEFAULT_REALM
     return func
+
+def rate_limit(func=None, realm=None):
+    '''Apply rate limiting directly to any view-like function.'''
+    if func is None:
+        return partial(rate_limit, realm=realm)
+
+    @wraps(func)
+    def _inner(request, *args, **kwargs):
+        result = RatedMiddleware().process_view(request, func, args, kwargs)
+        if result is None:
+            return func(request, *args, **kwargs)
+        return result
+
+    return _inner
 
