9a6ba7fd01260a6a042acf650494b8a87982cb1d
Curtis Maloney <curtis@tinbrain.net>
2023-08-21 17:26:10 +1000

    Start test project

---
+++ tests/settings.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+
+SECRET_KEY = 'dummy-secret-key'
+
+INSTALLED_APPS = [
+]
+
+DATABASES={
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+    }
+}
+
+ROOT_URLCONF='test.urls'
+
+USE_TZ = True  # Avoid deprecation warning
+
